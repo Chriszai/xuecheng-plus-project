39022d7500d156ec44e8fbc45121350b6f9f7fe9
MSI-PC <zaizai000816@gmail.com>
2023-04-04 15:45:07 +0100

    完成了对异常情况的处理

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -48,9 +48,9 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         //拼装查询条件
         LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
         //根据名称模糊查询,在sql中拼接 course_base.name like '%值%'
-        queryWrapper.like(StringUtils.isNotEmpty(courseParamsDto.getCourseName()),CourseBase::getName,courseParamsDto.getCourseName());
+        queryWrapper.like(StringUtils.isNotEmpty(courseParamsDto.getCourseName()), CourseBase::getName, courseParamsDto.getCourseName());
         //根据课程审核状态查询 course_base.audit_status = ?
-        queryWrapper.eq(StringUtils.isNotEmpty(courseParamsDto.getAuditStatus()), CourseBase::getAuditStatus,courseParamsDto.getAuditStatus());
+        queryWrapper.eq(StringUtils.isNotEmpty(courseParamsDto.getAuditStatus()), CourseBase::getAuditStatus, courseParamsDto.getAuditStatus());
         //todo:按课程发布状态查询
 
         //创建page分页参数对象，参数：当前页码，每页记录数
@@ -63,13 +63,13 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         long total = pageResult.getTotal();
 
         //List<T> items, long counts, long page, long pageSize
-        PageResult<CourseBase> courseBasePageResult = new PageResult<CourseBase>(items,total,pageParams.getPageNo(), pageParams.getPageSize());
-        return  courseBasePageResult;
+        PageResult<CourseBase> courseBasePageResult = new PageResult<CourseBase>(items, total, pageParams.getPageNo(), pageParams.getPageSize());
+        return courseBasePageResult;
     }
 
     @Transactional
     @Override
-    public CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto dto){
+    public CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto dto) {
 
         //参数的合法性校验
         if (StringUtils.isBlank(dto.getName())) {
@@ -106,7 +106,7 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
 //        courseBaseNew.setName(dto.getName());
 //        courseBaseNew.setDescription(dto.getDescription());
         //上边的从原始对象中get拿数据向新对象set，比较复杂
-        BeanUtils.copyProperties(dto,courseBaseNew);//只要属性名称一致就可以拷贝
+        BeanUtils.copyProperties(dto, courseBaseNew);//只要属性名称一致就可以拷贝
         courseBaseNew.setCompanyId(companyId);
         courseBaseNew.setCreateDate(LocalDateTime.now());
         //审核状态默认为未提交
@@ -115,14 +115,14 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         courseBaseNew.setStatus("203001");
         //插入数据库
         int insert = courseBaseMapper.insert(courseBaseNew);
-        if(insert<=0){
+        if (insert <= 0) {
             throw new RuntimeException("添加课程失败");
         }
 
         //向课程营销系courese_market写入数据
         CourseMarket courseMarketNew = new CourseMarket();
         //将页面输入的数据拷贝到courseMarketNew
-        BeanUtils.copyProperties(dto,courseMarketNew);
+        BeanUtils.copyProperties(dto, courseMarketNew);
         //课程的id
         Long courseId = courseBaseNew.getId();
         courseMarketNew.setId(courseId);
@@ -135,11 +135,11 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
     }
 
     //查询课程信息
-    public CourseBaseInfoDto getCourseBaseInfo(long courseId){
+    public CourseBaseInfoDto getCourseBaseInfo(long courseId) {
 
         //从课程基本信息表查询
         CourseBase courseBase = courseBaseMapper.selectById(courseId);
-        if(courseBase==null){
+        if (courseBase == null) {
             return null;
         }
         //从课程营销表查询
@@ -147,44 +147,43 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
 
         //组装在一起
         CourseBaseInfoDto courseBaseInfoDto = new CourseBaseInfoDto();
-        BeanUtils.copyProperties(courseBase,courseBaseInfoDto);
-        if(courseMarket!=null){
-            BeanUtils.copyProperties(courseMarket,courseBaseInfoDto);
+        BeanUtils.copyProperties(courseBase, courseBaseInfoDto);
+        if (courseMarket != null) {
+            BeanUtils.copyProperties(courseMarket, courseBaseInfoDto);
         }
 
         //通过courseCategoryMapper查询分类信息，将分类名称放在courseBaseInfoDto对象
         //todo：课程分类的名称设置到courseBaseInfoDto
 
         return courseBaseInfoDto;
-
     }
 
     //单独写一个方法保存营销信息，逻辑：存在则更新，不存在则添加
-    private int saveCourseMarket(CourseMarket courseMarketNew){
+    private int saveCourseMarket(CourseMarket courseMarketNew) {
 
         //参数的合法性校验
         String charge = courseMarketNew.getCharge();
-        if(StringUtils.isEmpty(charge)){
+        if (StringUtils.isEmpty(charge)) {
             throw new RuntimeException("收费规则为空");
         }
         //如果课程收费，价格没有填写也需要抛出异常
-        if(charge.equals("201001")){
-           if(courseMarketNew.getPrice() ==null || courseMarketNew.getPrice().floatValue()<=0){
+        if (charge.equals("201001")) {
+            if (courseMarketNew.getPrice() == null || courseMarketNew.getPrice().floatValue() <= 0) {
 //               throw new RuntimeException("课程的价格不能为空并且必须大于0");
-               XueChengPlusException.cast("课程的价格不能为空并且必须大于0");
-           }
+                XueChengPlusException.cast("课程的价格不能为空并且必须大于0");
+            }
         }
 
         //从数据库查询营销信息,存在则更新，不存在则添加
         Long id = courseMarketNew.getId();//主键
         CourseMarket courseMarket = courseMarketMapper.selectById(id);
-        if(courseMarket == null){
+        if (courseMarket == null) {
             //插入数据库
             int insert = courseMarketMapper.insert(courseMarketNew);
             return insert;
-        }else{
+        } else {
             //将courseMarketNew拷贝到courseMarket
-            BeanUtils.copyProperties(courseMarketNew,courseMarket);
+            BeanUtils.copyProperties(courseMarketNew, courseMarket);
             courseMarket.setId(courseMarketNew.getId());
             //更新
             int i = courseMarketMapper.updateById(courseMarket);
@@ -193,7 +192,6 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
 
 
     }
-
 
 
 }
